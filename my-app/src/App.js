c99c28a47db635f7d1437ab27731c9a5b37f3a0b
melvinjames <melvin.gacheru@student.morigaschool.com>
2023-05-01 19:07:03 +0300

    completed assignment

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BotArmy from './BotArmy';
+import BotCollection from './components/BotCollection';
+
+function App() {
+  return (
+    <div className="App">
+      <BotArmy />
+      <BotCollection/>
+    </div>
+  );
+}
+
+export default App;
